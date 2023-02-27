60cc56b220d2aad71927e5e8c0eb65e27ede4186
FoX <you@example.com>
2023-02-27 09:53:19 +0200

    htts => htts

--- src/redux/auth/authOperation.js
+++ src/redux/auth/authOperation.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://n-code.com.ua/';
+axios.defaults.baseURL = 'http://n-code.com.ua/';
 // axios.defaults.baseURL = 'http://localhost:3030/';
 
 export const setAuthHeader = token => {
